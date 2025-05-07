765b922eea965260f0ddd11533feae8b0c70a3b0
Pedro <eduba.oscar@gmail.com>
2025-05-07 20:31:04 -0300

    a

--- src/components/Drawer/DrawerHandle.vue
+++ src/components/Drawer/DrawerHandle.vue
@@ -1,13 +1,34 @@
 <script setup>
 
+  const isRotated = ref(false);
+
+  const toggle = () => {
+    console.log('toggle');
+    isRotated.value = !isRotated.value;
+  }
+
 </script>
 
 <template>
 
-  <v-btn rounded="xl">></v-btn>
+  <v-btn
+    class="soft-transition"
+    :class="{ reverse: isRotated }"
+    icon="fa:fa-solid fa-caret-right"
+    variant="flat"
+    @click="toggle"
+  />
 
 </template>
 
 <style scoped>
+
+  .soft-transition{
+    transition: transform 0.4s ease-out;
+  }
+
+  .reverse {
+    transform: rotate(180deg);
+  }
 
 </style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,13 +7,34 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import colors from 'vuetify/util/colors'
+import '@fortawesome/fontawesome-free/css/all.css'
+import { aliases, fa } from 'vuetify/iconsets/fa'
 
-// Composables
 import { createVuetify } from 'vuetify'
 
-// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+const myTheme = {
+  dark: false,
+  colors: {
+    background: colors.brown.lighten1,
+    surface: colors.blueGrey.lighten3,
+    primary: colors.brown.base,
+    secondary: colors.blueGrey.base,
+  },
+}
+
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: 'myTheme',
+    themes: {
+      myTheme,
+    },
+  },
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+    },
   },
 })
